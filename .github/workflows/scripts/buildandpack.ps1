e0828a4ca27fefafc9b9579de66722b5ade742e9
Joe Beernink <jbeernink@willowinc.com>
2023-06-28 15:46:32 -0700

    Trying secret as argument

--- .github/workflows/scripts/buildandpack.ps1
+++ .github/workflows/scripts/buildandpack.ps1
@@ -1,3 +1,4 @@
+param ($authToken)
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 Import-Module "$PSScriptRoot/common.psm1" -Force
@@ -18,7 +19,7 @@ foreach($solution in $(Get-Solutions)) {
             throw
         }
 
-        dotnet nuget push $rootPath/bin/Release/net6.0/Microsoft.SmartPlaces.Facilities.*.nupkg --api-key "$env:NUGET_AUTH_TOKEN" --source "$env:AZURE_ARTIFACTS_FEED_URL" --skip-duplicate
+        dotnet nuget push $rootPath/bin/Release/net6.0/Microsoft.SmartPlaces.Facilities.*.nupkg --api-key "$authToken" --source "$env:AZURE_ARTIFACTS_FEED_URL" --skip-duplicate
 
     pop-location
 }
